f4cd053d4ff605a5c61b24847aba803adb282531
komarp <pavel.komar.01@mail.ru>
2020-02-03 15:39:09 +0300

    1.py

--- lesson4/1.py
+++ lesson4/1.py
@@ -4,3 +4,16 @@
 # и частное (в любом случае).
 
 # add code here
+try:
+    a = int(input('Chislo1 >> '))
+    b = int(input('Chislo2 >> '))
+except ValueError:
+    print('Введите число!')
+else:
+    try:
+        if a % b is not False:
+            print('NO', a // b, '\n', 'ostatok >> ', a % b)
+        else:
+            print('YES', a // b)
+    except ZeroDivisionError:
+        print('Деление на 0!')
