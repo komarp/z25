7e3729a67ce0284c596e768305bed03e1b6e9298
komarp <pavel.komar.01@mail.ru>
2020-02-03 15:39:54 +0300

    3.py

--- lesson4/3.py
+++ lesson4/3.py
@@ -2,3 +2,16 @@
 # Найте элементы списка, которые меньше среднего арифметического.
 
 # add code here
+lst = []
+while True:
+    x = input('Chislo >> ')
+    if x == '':
+        break
+    else:
+        try:
+            lst.append(float(x))
+        except ValueError:
+            print('Vvedite chislo !')
+for num in lst:
+    if num < (sum(lst) / len(lst)):
+        print('Average > ', num)
