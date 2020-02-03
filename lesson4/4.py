0643e75434b75274e3e883f24dc89a95f6735c33
komarp <pavel.komar.01@mail.ru>
2020-02-03 15:40:14 +0300

    4.py

--- lesson4/4.py
+++ lesson4/4.py
@@ -2,3 +2,17 @@
 # в байты или килобайты в зависимости от его выбора.
 
 # add code here
+try:
+    num = float(input('Chislo >> '))
+except ValueError:
+    print('Vvedite chislo !')
+else:
+    _type = input('Vvedite - "k" for kilobytes or "b" for bytes: ')
+    if _type == 'b':
+        num = num * 1024
+        print('You num in kylobytes = ', num, 'bytes')
+    elif _type == 'k':
+        num = num / 1024
+        print('You num in bytes = ', num, 'kilobytes')
+    else:
+        print('Vvedite "k" or "b" !')
