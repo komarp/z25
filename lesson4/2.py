7c3ada78009e5b20c8e248b8e2c5057aa3322582
komarp <pavel.komar.01@mail.ru>
2020-02-03 15:39:35 +0300

    2.py

--- lesson4/2.py
+++ lesson4/2.py
@@ -2,3 +2,19 @@
 # Сколько элементов списка больше по модулю максимального числа.
 
 # add code here
+_max = 0
+counter = 0
+while True:
+    a = input('Chislo >> ')
+    if a == '':
+        break
+    else:
+        try:
+            if float(a) > _max:
+                _max = float(a)
+        except ValueError:
+            print('Ошибка конвертирвания, введите число!')
+        else:
+            if abs(float(a)) > _max:
+                counter += 1
+print(counter)
