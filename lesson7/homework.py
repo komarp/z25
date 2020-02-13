95855ddbc81899b38b26d3232a8ca1a572f3ac29
komarp <pavel.komar.01@mail.ru>
2020-02-13 13:07:38 +0300

    lesson7_komar

--- lesson7/homework.py
+++ lesson7/homework.py
@@ -1,3 +1,7 @@
+import re
+import time
+
+
 """1. Написать кэширующий декоратор,
 который принимает время (в секундах, сколько необюходимо хранить результат)
 
@@ -7,9 +11,21 @@ def foo():
 """
 
 
-def cache(*args, **kwargs):
-    pass
-
+def cache(seconds):
+    def inner(func):
+        def decorator(*args, **kwargs):
+            if not hasattr(cache, '_cache'):
+                cache._cache = {}
+            cache_key = (args)
+            res = cache._cache.get(cache_key)
+            if not res:
+                res = func(*args, **kwargs)
+                cache._cache[cache_key] = res, time.time()
+            if time.time()- cache._cache[cache_key][1] >= seconds:
+                delattr(cache._cache, cache_key)
+            return res
+        return decorator
+    return inner
 
 """
 2.
@@ -36,8 +52,22 @@ def foo1():
 """
 
 
-def counter(*args, **kwargs):
-    pass
+def counter(str):
+    def inner(func):
+        def decorator(*args, **kwargs):
+            if not hasattr(counter, '_cnt'):
+                counter._cnt = 1
+            else:
+                counter._cnt += 1
+            print(f'{str} - {counter._cnt}')
+            return func(*args, **kwargs)
+        return decorator
+    return inner
+
+
+@counter('Text')
+def foo():
+    return 1
 
 
 """
@@ -51,8 +81,15 @@ the printing 10-13-2018 and typesetting industry.
 """
 
 
-def get_datetimes(*args, **kwargs):
-    pass
+text = '''Lorem Ipsum is simply 12-01-2018 dummy text of
+the printing 10-13-2018 and typesetting industry.
+10-02-2018 Lorem Ipsum has been the industry a s x'''
+
+
+def get_datetimes(txt):
+    pattern = re.compile('\d{2}-\d{2}-\d{4}')
+    return pattern.findall(txt)
+
 
 
 """
@@ -68,8 +105,16 @@ get_words('Lorem Ipsum is simply', sym=('vowels',))
 """
 
 
-def get_words(*args, **kwargs):
-    pass
+text2 = 'Lorem Ipsum is simply'
+
+
+def get_words(txt, code=None):
+    if code == 'consonants':
+        return re.compile(r'\b[^aeouiAEOUI\d -.\n]\w*').findall(txt)
+    elif code == 'vowels':
+        return re.compile(r'\b[aeouiAEOUI]\w+\b').findall(txt)
+    else:
+        return txt
 
 
 """
@@ -85,9 +130,27 @@ get_result(s)
 """
 
 
-def get_ping_info(*args, **kwargs):
-    pass
+s = """64 bytes from 216.58.215.110: icmp_seq=0 ttl=54 time=30.391 ms
+64 bytes from 216.58.215.110: icmp_seq=1 ttl=54 time=30.667 ms
+64 bytes from 216.58.215.110: icmp_seq=2 ttl=54 time=33.201 ms
+64 bytes from 216.58.215.110: icmp_seq=3 ttl=54 time=30.140 ms
+64 bytes from 216.58.215.110: icmp_seq=4 ttl=54 time=31.822 ms"""
+
+
+def get_ping_info(text):
+    lst = re.compile(r'\b\d{1}\b').findall(text)
+    lst2 = re.compile(r'\d{2}.\d{3}[^.:]').findall(text)
+    return tuple(zip(list(map(int,lst)), list(map(float,lst2))))
 
 
 if __name__ == "__main__":
     pass
+    assert foo() ==  1
+    print('counter - OK')
+    assert get_datetimes(text) == ['12-01-2018', '10-13-2018', '10-02-2018']
+    print('get_datetimes - OK')
+    assert get_words(text2, code='vowels') == ['Ipsum', 'is']
+    assert get_words(text2, code='consonants') == ['Lorem', 'simply']
+    assert get_words(text2) =='Lorem Ipsum is simply'
+    print('get_words - OK')
+    assert get_ping_info(s) == ((0, 30.391), (1, 30.667), (2, 33.201), (3, 30.14), (4, 31.822))
