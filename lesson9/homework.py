c7b0af99af9800e11b55f9e89267b996efb270e7
komarp <pavel.komar.01@mail.ru>
2020-02-20 22:29:32 +0300

    lesson9_komar

--- lesson9/homework.py
+++ lesson9/homework.py
@@ -13,3 +13,78 @@ https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
 - выдать сообщение об ошибке, если в полный стек добавить элемент нельзя,
 или из пустого стека достать элемент
 """
+
+
+ ############## Class Queue ###################
+class Queue:
+    def  __init__(self, width=None):
+        self.__width  = width
+        self.__value = []
+
+
+    def show_me(self):
+        print('Width is', self.__width, ';', 'Value is', str(self.__value))
+
+
+    def __perm_to_add(self):
+        if self.__width is None:
+            return True
+        elif self.__width > len(self.__value):
+            return True
+        else:
+            return False
+
+
+    def add(self, obj):
+        if self.__perm_to_add():
+            self.__value.append(obj)
+        else:
+            raise Exception('Too many obj')
+
+
+    def __checknull(self):
+            return True if len(self.__value) > 0 else False
+
+
+    def remove(self):
+        if self.__checknull():
+            del self.__value[0]
+        else:
+            raise Exception('Nothing to delete!')
+
+
+ #################### Class Stack################
+
+class Stack:
+    def __init__(self, size=None):
+        self.__size = size
+        self.__value = []
+
+    def show_me(self):
+        print('Available size is', self.__size, 'Value is', self.__value)
+
+    def __perm_to_add(self):
+        if self.__size is None:
+            return True
+        elif self.__size > len(self.__value):
+            return True
+        else:
+            return False
+
+    def add(self, obj):
+        if self.__perm_to_add():
+            self.__value.append(obj)
+        else:
+            raise Exception('Too many objects!')
+
+    def __check_it_out(self):
+        return False if len(self.__value) == 0 else True
+
+    def get(self):
+        if self.__check_it_out():
+            a = self.__value[-1]
+            del self.__value[-1]
+            return a
+        else:
+            raise Exception('Nothing to get!')
+
