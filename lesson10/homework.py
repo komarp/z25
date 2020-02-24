6237bcbbf83a34e9e318ba0f6b152811ce45c696
komarp <pavel.komar.01@mail.ru>
2020-02-25 00:05:15 +0300

    lesson10_komar

--- lesson10/homework.py
+++ lesson10/homework.py
@@ -1,3 +1,5 @@
+import time
+
 """
 0. (*) Написать функцию, которая из списка чисел составляет
 максимальное число
@@ -6,7 +8,7 @@
 
 
 def max_number(_list):
-    pass
+    return ''.join(sorted(list(map(str, _list)), key=lambda x: x[0])[::-1])
 
 
 """
@@ -15,6 +17,19 @@ def max_number(_list):
 несколькими файлами:
 MultiFileOpen(('file1.txt', 'r'), ('file2.txt', 'w'), ..., ('fileN.txt', 'rb'))
 """
+
+
+class MultiFileOpen:
+    def __init__(self, file_name, meth):
+        self.file_obj = open(file_name, meth)
+
+    def __enter__(self):
+        return self.file_obj
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print(repr(exc_tb), repr(exc_val), repr(exc_type))
+        self.file_obj.close()
 
 
 """
@@ -27,6 +42,22 @@ with Timer("Time: {}") as timer:
     do_some_other_logic()
     print(timer.now())  # Time: 5.71 sec
 """
+
+
+class Timer:
+    def __init__(self, _str):
+        self._str = _str
+        self.start = time.time()
+
+    def __enter__(self):
+        return self
+
+    def now(self):
+        return self._str.format(self.start - time.time())
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print(repr(exc_type), repr(exc_tb), repr(exc_val))
 
 
 if __name__ == '__main__':
