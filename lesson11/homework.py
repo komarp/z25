0d3908cc1e658ce6d77aff57e7629d8a53ebbf42
komarp <pavel.komar.01@mail.ru>
2020-03-01 17:51:11 +0300

    lesson11_komar

--- lesson11/homework.py
+++ lesson11/homework.py
@@ -5,6 +5,22 @@ n включительно.
 """
 
 
+class Fib:
+    def __init__(self, num):
+        self.num = num
+        self.cnt = 0
+
+    def __next__(self):
+        lst = [1, 1]
+        while lst[-1] <= self.num:
+            lst.append(lst[-1] + lst[-2])
+        if lst[self.cnt] <= self.num:
+            phase = self.cnt
+            self.cnt += 1
+            return lst[phase]
+        raise StopIteration
+
+
 """
 2.
 Напишите класс, объектом которого будет итератор производящий только
@@ -12,11 +28,44 @@ n включительно.
 """
 
 
+class Even:
+    def __init__(self, num):
+        self.num = num
+        self.start = 0
+
+    def __next__(self):
+        if self.start <= self.num:
+            run = self.start
+            self.start += 2
+            return run
+        raise StopIteration
+
+
 """
 3.
 Напишите итератор factorials(n), генерирующий последовательность
 факториалов натуральных чисел.
 """
+
+
+class Factorials:
+    def __init__(self, num):
+        self.num = num
+        self.cnt = 0
+
+    def __next__(self):
+        lst = []
+        val = 1
+        for i in range(1, self.num + 1):
+            val *= i
+            lst.append(val)
+        if self.cnt <= len(lst):
+            phase = self.cnt
+            self.cnt += 1
+            try:
+                return lst[phase]
+            except IndexError:
+                raise StopIteration
 
 
 """
--- lesson11/tests.py
+++ lesson11/tests.py
@@ -1,9 +1,39 @@
 import unittest
+from homework import Fib, Even, Factorials
 
 
 class HomeworkTest(unittest.TestCase):
-    def test_ok(self):
-        self.assertTrue(True)
+    def test_Fib(self):
+        a = Fib(5)
+        self.assertTrue(a)
+        self.assertEqual(next(a), 1)
+        self.assertEqual(next(a), 1)
+        self.assertEqual(next(a), 2)
+        self.assertEqual(next(a), 3)
+        self.assertEqual(next(a), 5)
+        with self.assertRaises(StopIteration):
+            next(a)
+
+    def test_Even(self):
+        b = Even(6)
+        self.assertTrue(b)
+        self.assertEqual(next(b), 0)
+        self.assertEqual(next(b), 2)
+        self.assertEqual(next(b), 4)
+        self.assertEqual(next(b), 6)
+        with self.assertRaises(StopIteration):
+            next(b)
+
+    def test_Factorials(self):
+        c = Factorials(5)
+        self.assertTrue(c)
+        self.assertEqual(next(c), 1)
+        self.assertEqual(next(c), 2)
+        self.assertEqual(next(c), 6)
+        self.assertEqual(next(c), 24)
+        self.assertEqual(next(c), 120)
+        with self.assertRaises(StopIteration):
+            next(c)
 
 
 if __name__ == '__main__':
